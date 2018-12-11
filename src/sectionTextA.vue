d619172ce586c0a18c094123d26f58ccb7ba6789
Dams-d <damien.duro@epitech.eu>
2018-12-11 20:31:12 +0000

    init section files

---
+++ src/sectionTextA.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="text_A">
+        <!-- wwManager:start -->
+        <wwSectionEditMenu v-bind:sectionCtrl="sectionCtrl"></wwSectionEditMenu>
+        <!-- wwManager:end -->
+
+        <wwObject class="background" v-bind:ww-object="section.data.background" ww-category="background"></wwObject>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "text_A",
+    props: {
+        sectionCtrl: Object
+    },
+    data() {
+        return {}
+    },
+    computed: {
+        section() {
+            return this.sectionCtrl.get();
+        }
+    },
+    created() {
+        this.section.data = this.section.data || {};
+
+        //Initialize image
+        if (!this.section.data.background) {
+            this.section.data.background = wwLib.wwObject.getDefault({ type: 'ww-image' });
+        }
+
+    },
+    methods: {}
+};
+</script>
+
+<style scoped>
+.text_A {}
+
+.text_A .background {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+}
+</style>
